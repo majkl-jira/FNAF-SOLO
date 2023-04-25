d04874cfc703bde11296390d234c176fd19dd221
Majkl <michaljira24@gmail.com>
2023-04-25 15:30:04 +0200

    Uprava ubrazků kai na kamerách a chodbách

--- res/js/game.js
+++ res/js/game.js
@@ -2,11 +2,13 @@ const left = document.getElementById('clickleft');
 const start = document.getElementById('startpoint');
 const right = document.getElementById('clickright');
 const exit= document.getElementById('clickexit');
-const kam1= document.getElementById('kam1');
-const kam2= document.getElementById('kam2');
+const kam= document.getElementById('kam');
 const chodbaL= document.getElementById('chodbaL');
 const chodbaR= document.getElementById('chodbaR');
 const bg= document.getElementById('bg');
+const time= document.getElementById('time');
+const timehod= document.getElementById('timehod');
+const pychhod= document.getElementById('pychhod');
 
 let goBack 
 
@@ -18,7 +20,7 @@ doorOpen(left, dirArr1, start, chodbaL, 2)
 
 function doorOpen(direction, arr, mainbg, chodba, val) {
     direction.onclick = () => {
-        inRoom = val;
+        inRoom = 2;
         arr.forEach(element => {
             element.style.display = "none";
         });
