f0835836a0de1e0dd67867433b72bf23ccfb94dd
Majkl <michaljira24@gmail.com>
2023-04-26 12:15:15 +0200

    Vzhled-Done

--- res/js/game.js
+++ res/js/game.js
@@ -9,27 +9,42 @@ const bg = document.getElementById("bg");
 const time = document.getElementById("time");
 const timehod = document.getElementById("timehod");
 const pychhod = document.getElementById("pychhod");
+const camClick1 = document.getElementById("camClick1");
+const camClick2 = document.getElementById("camClick2");
+const camDiv = document.getElementById("camDiv");
+const kam1withKai = document.getElementById("kam1withKai");
+
+highlightBg(left)
+highlightBg(right)
 
 let goBack;
 
 let inRoom = 1;
 
-const dirArr1 = [left, right, exit];
+const dirArr1 = [left, right, exit, camDiv];
+const dirArr2 = [left, right, exit];
 
-doorLeft(left, dirArr1, start, chodbaL);
 
-function doorLeft(direction, arr, mainbg, chodba) {
+doorDir(left, dirArr1, start, chodbaL, 2);
+doorDir(right, dirArr1, start, chodbaR, 3);
+camDir();
+
+
+function doorDir(direction, arr, mainbg, chodba, inRoomVal) {
   direction.onclick = () => {
-    inRoom = 2;
+    inRoom = inRoomVal;
     arrDisplay(arr, mainbg, chodba, "none", "block")
+    goBack = document.createElement("div");
+    goBack.style.width = "12%";
+    goBack.style.height = "60%";
+    goBack.style.display = "block";
+    goBack.style.zIndex = "5";
+    goBack.style.position = "absolute";
+    goBack.style.cursor = "pointer";
+
+    highlightBg(goBack);
+
     if (inRoom == 2) {
-      goBack = document.createElement("div");
-      goBack.style.width = "12%";
-      goBack.style.height = "60%";
-      goBack.style.backgroundColor = "red";
-      goBack.style.display = "block";
-      goBack.style.zIndex = "5";
-      goBack.style.position = "absolute";
       goBack.style.top = "30%";
       goBack.style.right = "15%";
 
@@ -37,6 +52,21 @@ function doorLeft(direction, arr, mainbg, chodba) {
 
       goBack.onclick = () => {
         arrDisplay(arr, mainbg, chodba, "block", "none")
+        goBack.style.display = "none";
+      }
+    }
+
+    if (inRoom == 3) {
+      goBack.style.bottom = "0%";
+      goBack.style.left = "33%";
+      goBack.style.width = "35%";
+      goBack.style.height = "20%";
+
+      bg.appendChild(goBack);
+
+      goBack.onclick = () => {
+        arrDisplay(arr, mainbg, chodba, "block", "none")
+        goBack.style.display = "none";
       }
     }
   };
@@ -48,4 +78,24 @@ function arrDisplay(array, div, div2, first, second){
   });
   div.style.display = first;
   div2.style.display = second;
+}
+
+function camDir(){
+  camClick2.onclick = () => {
+    arrDisplay(dirArr2, start, kam1withKai,  "none", "block")
+  }
+  camClick1.onclick = () => {
+    arrDisplay(dirArr2, start, kam1withKai,  "block", "none")
+  }
+}
+
+function highlightBg(highDiv){
+  highDiv.style.backgroundColor = "transparent";
+  highDiv.onmouseenter = () => {
+    highDiv.style.backgroundColor = "rgba(236, 57, 41, 0.35)";
+  }
+
+  highDiv.onmouseleave = () => {
+    highDiv.style.backgroundColor = "transparent";
+  }
 }
